dd593fe7b5dd65c59da9361b926de065525ed5f3
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2024-12-05 10:41:48 +0100

    v

--- T3SOCKETS/EJEMPLOS/src/main/java/socketDatagram/EmisorDatagram.java
+++ T3SOCKETS/EJEMPLOS/src/main/java/socketDatagram/EmisorDatagram.java
@@ -1,16 +1,19 @@
 package socketDatagram;
 
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public class EmisorDatagram {
 
 	public static void main(String[] args) throws IOException {
 		try {
 			System.out.println("Creando socket datagram");
-			DatagramSocket datagramsocket = new DatagramSocket();
+                        InetSocketAddress addrOwn = new InetSocketAddress ("localhost", 5556);
+			DatagramSocket datagramsocket = new DatagramSocket(addrOwn);
 			System.out.println("Enviando mensaje");
 			
 			String mensaje = "mensaje desde el emisor";
@@ -21,6 +24,14 @@ public class EmisorDatagram {
 			
 			datagramsocket.send(datagrama_);
 			System.out.println("Mensaje enviado");
+                        System.out.println("Recibiendo mensaje");
+			
+			byte[] mensajeByte = new byte[25];
+			DatagramPacket datagrama1 = new DatagramPacket (mensajeByte,mensajeByte.length);
+			datagramsocket.receive(datagrama1);//se bloquea hasta que recibe un mensaje
+			
+			System.out.println("Mensaje recibido: " + new String(mensajeByte));
+                        
 			System.out.println("Cerrando el socket datagrama");
 			datagramsocket.close();
 			

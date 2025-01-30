ea1f209c29af696d5aa9e9ea57a227951ebb97de
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2025-01-30 14:13:46 +0100

    uy

---
+++ UT4ProgramacionDeServicios/ftpExample/src/main/java/maestre/FTPExample4.java
@@ -0,0 +1,40 @@
+package maestre;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.IOException;
+
+public class FTPExample4 {
+	public static void main(String[] args) {
+		FTPClient ftpClient = null;
+		try {
+			ftpClient = new FTPClient();
+			ftpClient.connect("localhost", 21);
+			if (ftpClient.login("prueba", "prueba")) {
+				System.out.println("Conectado al servidor FTP");
+				// Usar pasive mode
+				ftpClient.enterLocalPassiveMode();
+
+				// listar los archivos en un directorio
+				FTPFile[] files = ftpClient.listFiles("/");
+				for (FTPFile file : files) {
+					System.out.println("Nombre: " + file.getName());
+				}
+			} else {
+				System.out.println("Login fallido");
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
